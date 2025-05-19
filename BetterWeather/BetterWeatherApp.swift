e932a39a55e18f7ec3cc6b577cfecbe8462a3bb8
Noah Mattocks <noahmattocks@gmail.com>
2025-05-19 16:05:03 -0400

    Initial Commit

---
+++ BetterWeather/BetterWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterWeatherApp.swift
+//  BetterWeather
+//
+//  Created by Owner on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
